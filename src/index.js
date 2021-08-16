8d70d09f9aae3c3f7e56a589f4b1d05c7232e06e
demonicirfan <hashtag.irfan@gmail.com>
2021-08-16 17:35:06 +0530

    class components

--- src/index.js
+++ src/index.js
@@ -1,25 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-if (module.hot) {
-  module.hot.accept();
-}
-
 class App extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = { lat: null };
+    this.state = { lat: 40 };
+
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => {
+        //we called setstate
+        this.setState({ lat: position.coords.latitude });
+      },
+      (err) => console.log(err)
+    );
   }
 
   // React says we have to define render!!
   render() {
-    window.navigator.geolocation.getCurrentPosition(
-      (position) => console.log(position),
-      (err) => console.log(err)
-    );
-
-    return <div>Latitude: </div>;
+    return <div>Latitude: {this.state.lat}</div>;
   }
 }
 
